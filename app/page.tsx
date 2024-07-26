9d14b5fb454a6744260e788c150ab64727a9148a
git-chad <tobimocc@gmail.com>
2024-07-26 15:53:11 -0300

    lrccdsm

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Messages from "@/components/chat/messages";
 export default function Home() {
   return (
     <main className="min-h-screen w-full container flex justify-center">
-      <Messages />
+      <Messages isAdminChat={true}/>
     </main>
   );
 }
